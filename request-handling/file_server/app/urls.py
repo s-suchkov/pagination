27cb469b399259c92c99359a61eb6274c5efd1b3
Sergey <ser3421@gmail.com>
2019-08-08 13:35:06 +0400

    twowork

--- request-handling/file_server/app/urls.py
+++ request-handling/file_server/app/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 
 # Определите и зарегистрируйте конвертер для определения даты в урлах и наоборот урла по датам
 
-
+from app.views import file_list, file_content
 urlpatterns = [
     # Определите схему урлов с привязкой к отображениям .views.file_list и .views.file_content
-    # path(..., name='file_list'),
-    # path(..., name='file_list'),    # задайте необязательный параметр "date"
+    path('', file_list, name='file_list'),
+    path('<date>/', file_list, name='file_list'),    # задайте необязательный параметр "date"
                                       # для детальной информации смотрите HTML-шаблоны в директории templates
-    # path(..., name='file_content'),
+    path('files/<name>', file_content, name='file_content'),
 ]
--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -1,29 +1,55 @@
 import datetime
-
+import os
+from django.http import HttpResponse
+from app.settings import FILES_PATH
 from django.shortcuts import render
+print(os.listdir(FILES_PATH))
+print(os.stat(FILES_PATH))
 
-
-def file_list(request):
+# print(os.stat('settings.py'))
+def file_list(request, date=None):
     template_name = 'index.html'
-    
+    # print(os.listdir('settings.FILES_PATH'))
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
     context = {
         'files': [
-            {'name': 'file_name_1.txt',
-             'ctime': datetime.datetime(2018, 1, 1),
-             'mtime': datetime.datetime(2018, 1, 2)}
-        ],
-        'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
+            # {'name': i,
+            #  'ctime': datetime.datetime.fromtimestamp(stat.st_ctime),
+            #  'mtime': datetime.datetime.fromtimestamp(stat.st_mtime)}
+        ]
+        # 'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
     }
+    context_1 = {
+        'files': []
+        # 'date': datetime.datetime.strptime(date, '%Y-%M-%d').date()
+    }
+    for i in os.listdir(FILES_PATH):
+        list = os.path.join(FILES_PATH, i)
+        stat = os.stat(list)
+        file = {'name': i,
+         'ctime': datetime.datetime.fromtimestamp(stat.st_ctime),
+         'mtime': datetime.datetime.fromtimestamp(stat.st_mtime)}
+        context['files'].append(file)
+        if date!= None:
+            date_res = file['mtime'].date()
+            # return HttpResponse(f'{date_res}, {date} {date_res==date}')
+            if str(date) == str(date_res):
+                context_1['files'].append(file)
+    if date != None:
+        context_1['date'] = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+        return render(request, template_name, context_1)
 
     return render(request, template_name, context)
-
+# print(datetime.date('2018-01-01'))
 
 def file_content(request, name):
+    server = os.path.join(FILES_PATH, name)
+    with open(server, 'r') as file:
+        content = file.read()
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
     return render(
         request,
         'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
+        context={'file_name': name, 'file_content': content}
     )
 
